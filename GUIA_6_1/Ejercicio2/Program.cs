49c38b670545d5a122d88121999822c30f03b547
adlfvrr <adolfover4@gmail.com>
2025-09-04 15:23:08 -0300

    -

--- GUIA_6_1/Ejercicio2/Program.cs
+++ GUIA_6_1/Ejercicio2/Program.cs
@@ -4,7 +4,7 @@ namespace Ejercicio2
 {
     public class Program
     {
-        private static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             string query = @"
 SELECT f.Id, f.Tipo, f.Area, f.Ancho, f.Largo, f.Radio
@@ -12,6 +12,20 @@ FROM Figuras f
 ";
             string stringDeConexion = "Data Source=172.16.3.41;Initial Catalog=Guia6_1_Ejercicio1_adolf_db;User ID=admindb;Password=admindb;Pooling=False;Connect Timeout=30;Encrypt=False;Authentication=SqlPassword;Application Name=vscode-mssql;Application Intent=ReadWrite;Command Timeout=30";
             using SqlConnection conexion = new SqlConnection(stringDeConexion);
+            await conexion.OpenAsync();
+
+            SqlCommand cmd = new SqlCommand(query, conexion);
+
+            SqlDataReader dataReader = await cmd.ExecuteReaderAsync();
+
+            while (await dataReader.ReadAsync())
+            {
+                int id = Convert.ToInt32(dataReader["Id"]);
+                int Tipo = Convert.ToInt32(dataReader["Tipo"]);
+                double? area = dataReader["Area"]!=DBNull.Value ? Convert.ToDouble(dataReader["Tipo"]) : null;
+
+                Console.WriteLine($"{id,10} | {Tipo,10} | {area,10}");
+            } 
         }
     }
 }
