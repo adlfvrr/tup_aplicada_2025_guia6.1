9a5717510392ecc61aa25946fea9e1b5795f1967
adlfvrr <adolfover4@gmail.com>
2025-09-04 15:03:56 -0300

    -

--- GUIA_6_1/Ejercicio1_base/2_Ejercicio2_query.sql
+++ GUIA_6_1/Ejercicio1_base/2_Ejercicio2_query.sql
@@ -4,6 +4,7 @@ USE Guia6_1_Ejercicio1_adolf_db
 
 GO
 
-SELECT * FROM Figuras
+SELECT f.Id, f.Tipo, f.Area, f.Ancho, f.Largo, f.Radio
+FROM Figuras f
 
 GO
---
+++ GUIA_6_1/Ejercicio2/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Ejercicio2
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string query = @"
+SELECT f.Id, f.Tipo, f.Area, f.Ancho, f.Largo, f.Radio
+FROM Figuras f
+";
+            string stringDeConexion = "Data Source=172.16.3.41;Initial Catalog=Guia6_1_Ejercicio1_adolf_db;User ID=admindb;Password=admindb;Pooling=False;Connect Timeout=30;Encrypt=False;Authentication=SqlPassword;Application Name=vscode-mssql;Application Intent=ReadWrite;Command Timeout=30";
+            using SqlConnection conexion = new SqlConnection(stringDeConexion);
+        }
+    }
+}
